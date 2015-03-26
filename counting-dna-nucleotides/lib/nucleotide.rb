4d86cd1f0df63084d377f3984a4bfa9b450c62a6
Kel Kanhirun <kkanhirun@gmail.com>
2015-03-25 20:55:48 -0500

    feat: Nucleotide has a sequence

---
+++ counting-dna-nucleotides/lib/nucleotide.rb
@@ -0,0 +1,7 @@
+class Nucleotide
+  attr_accessor :sequence
+
+  def initialize(sequence)
+    @sequence = sequence
+  end
+end
---
+++ counting-dna-nucleotides/spec/nucleotide_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Nucleotide do
+  let(:nucleotide) { Nucleotide.new('AGCTTTTCATTCTGAC') }
+
+  describe '#sequence' do
+    describe 'should return the argument passed' do
+      it 'should return AGCTTTTCATTCTGAC as its sequence' do
+        nucleotide = Nucleotide.new('AGCTTTTCATTCTGAC')
+
+        expect(nucleotide.sequence).to eql('AGCTTTTCATTCTGAC')
+      end
+
+      it 'should return CATTCT as its sequence' do
+        nucleotide = Nucleotide.new('CATTCT')
+
+        expect(nucleotide.sequence).to eql('CATTCT')
+      end
+    end
+  end
+
+  it 'should validate `sequence`'
+
+  describe '#frequencies' do
+    xit 'should return a Hash with nucleotides as keys and their count as values' do
+      results = nucleotide.frequencies()
+
+      expect(results).to eql({A: 3, C: 4, T: 7, G: 2})
+    end
+  end
+end
